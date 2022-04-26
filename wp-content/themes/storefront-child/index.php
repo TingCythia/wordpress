43cb054bb2715fc6655e7ece77349d2755bd6129
TingCythia <91254839+TingCythia@users.noreply.github.com>
2022-04-26 21:29:45 +0200

    commit new branch

--- wp-content/themes/storefront-child/index.php
+++ wp-content/themes/storefront-child/index.php
@@ -2,10 +2,10 @@
 echo "Startsida";
 ?>
 
-<form action='http://localhost/wordpress-5.9.2/wordpress/wp-admin/admin-ajax.php'>
+<form action='http://localhost/labb2-Ting/wp-admin/admin-ajax.php'>
     <input name="firstname" type="text">
     <input name="lastname" type="text">
-    <button type="summit" value="skicka">skikca</button>
+    <button type="summit" value="skicka">skicka</button>
 	<input type="hidden" name="action" value="skicka-form">
 
 </form>
